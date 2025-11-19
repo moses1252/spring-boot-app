5ebadc5f62117186618a5042755c7771797fced9
moses1252 <amviramontes34@yahoo.com>
2025-11-18 18:43:07 -0800

    added crud operations

--- src/main/java/com/amigoscode/SoftwareEngineerController.java
+++ src/main/java/com/amigoscode/SoftwareEngineerController.java
@@ -10,19 +10,27 @@ import java.util.List;
 @RequestMapping("api/v1/software-engineers")
 public class SoftwareEngineerController {
 
+    private final SoftwareEngineerService softwareEngineerService;
+
+    public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService) {
+        this.softwareEngineerService = softwareEngineerService;
+    }
+
     @GetMapping
     public List<SoftwareEngineer> getEngineers() {
-        return List.of (
-                new SoftwareEngineer(
-                1,
-                "James",
-                "js, node, react, tailwindcss"
-        ),
-        new SoftwareEngineer(
-                2,
-                "Jamilia",
-                "Java, Spring, spring Boot"
-        )
-        );
-    };
+        return softwareEngineerService.getAllSoftwareEngineers();
+
+    }
 }
+//                List.of ( we are updating this because this was to temporarily supposed to fill data
+//                new SoftwareEngineer(
+//                1,
+//                "James",
+//                "js, node, react, tailwindcss"
+//        ),
+//        new SoftwareEngineer(
+//                2,
+//                "Jamilia",
+//                "Java, Spring, spring Boot"
+//        )
+//        );
---
+++ src/main/java/com/amigoscode/SoftwareEngineerService.java
@@ -0,0 +1,20 @@
+package com.amigoscode;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+//this class will handle buniess logic
+@Service
+public class SoftwareEngineerService {
+
+    private final SoftwareEngineerRepository softwareEngineerRepository;
+
+    public SoftwareEngineerService(SoftwareEngineerRepository softwareEngineerRepository) {
+        this.softwareEngineerRepository = softwareEngineerRepository;
+    }
+
+    public List<SoftwareEngineer> getAllSoftwareEngineers() {
+        return softwareEngineerRepository.findAll();
+    }
+}
